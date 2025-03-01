33152fd70d6e479900292268510752c238a1548b
Piyal Shah <piyalsha007@gmail.com>
2025-03-01 16:24:34 +0600

    background color change functionality added done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
-console.log("connected")
+
+const btn = document.getElementById("theme-btn");
+btn.addEventListener('click', function () {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const alpha = 0.2;
+    const rgba = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    console.log(rgba);
+    document.getElementById("body").style.backgroundColor = rgba;
+});
